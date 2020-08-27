093499f0d0951b623b4fbfe70eef93eb32ba1764
Zil Vyas <zvyas08@gmail.com>
2020-08-27 09:42:05 -0400

    updates

--- restful-web-services/src/main/java/com/todo/rest/webservices/restfulwebservices/basic/auth/BasicAuthenticationController.java
+++ restful-web-services/src/main/java/com/todo/rest/webservices/restfulwebservices/basic/auth/BasicAuthenticationController.java
@@ -2,7 +2,6 @@ package com.todo.rest.webservices.restfulwebservices.basic.auth;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins="http://localhost:4200")
